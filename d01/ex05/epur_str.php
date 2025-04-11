7a1e8ef7cdd60e3b6a09f3b7f9efdde45b17ec14
hicham-darw <hamoudihicham@gmail.com>
2025-04-11 10:29:12 +0100

    fix epur_str.php

---
+++ d01/ex05/epur_str.php
@@ -0,0 +1,26 @@
+<?php
+	if($argc != 2)
+		exit;
+	$dest = $argv[1];
+	$dest = trim($dest);
+	$origin = null;
+	
+	while($pos = strpos($dest, " "))
+	{
+		if( !$origin)
+		{
+			$origin = substr($dest, 0, $pos);
+			$origin = $origin . " ";
+		}
+		else
+		{
+			$origin = $origin . substr($dest, 0, $pos);
+			$origin = $origin . " ";
+		}
+		$dest = substr($dest, $pos+1);
+		$dest = trim($dest);
+	}
+	$origin = $origin . $dest;
+	echo $origin . "\n";
+	
+?>
