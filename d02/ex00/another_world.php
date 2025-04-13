801d4534ecdb3aced3ebfef14d5861120606a42c
hicham-darw <hamoudihicham@gmail.com>
2025-04-13 02:18:47 +0100

    finish ex00 another world replace whitespaces

--- d02/ex00/another_world.php
+++ d02/ex00/another_world.php
@@ -2,24 +2,7 @@
 	if($argc == 1)
 		exit;
 	$str = trim($argv[1]);
-	$new = null;
-	while($pos = strpos($str, " "))
-	{	
-		if($new == null)
-		{
-			$tmp = substr($str, 0, $pos);
-			$new = substr($str, 0, $pos);
-		}
-		else
-		{
-			$tmp = substr($str, 0, $pos);
-			$new = $new . substr($str, 0, $pos);
-		}
-		$new = $new . " ";
-		$str = substr($str, $pos + 1);
-		$str = trim($str);
-	}
-	
-	$new = trim($new . $str);
-	echo $new . "\n";
+	$pattern = "/\s+/";
+	$str = preg_replace($pattern, " ", $str);
+	echo $str ;
 ?>	
