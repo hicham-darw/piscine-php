a0dd97693e5a6cb43ede0f9d6cfbd4888f1ff974
hicham-darw <hamoudihicham@gmail.com>
2025-05-03 05:45:30 +0100

    add table ft_table

---
+++ d05/ex01/ex01.sql
@@ -0,0 +1,8 @@
+USE db_darwin;
+
+CREATE TABLE ft_table (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	login VARCHAR (255) DEFAULT 'toto' NOT NULL,
+	`group` ENUM('staff', 'student', 'other') NOT NULL,
+	creation_date DATE NOT NULL
+);
