f39b2f892b20a0b37ce53b74f66464c96e38b883
hicham-darw <hamoudihicham@gmail.com>
2025-05-03 05:50:57 +0100

    fill table

---
+++ d05/ex02/ex02.sql
@@ -0,0 +1,16 @@
+USE db_darwin;
+
+INSERT INTO ft_table (login, `group`, creation_date)
+VALUES ("LOKI", "staff", "2023-05-01");
+
+INSERT INTO ft_table (login, `group`, creation_date)
+VALUES ("scadoux", "student", "2014-01-01");
+
+INSERT INTO ft_table (login, `group`, creation_date)
+VALUES ("chap", "staff", "2011-04-27");
+
+INSERT INTO ft_table (login, `group`, creation_date)
+VALUES ("bambo", "staff", "2014-03-01");
+
+INSERT INTO ft_table (login, `group`, creation_date)
+VALUES ("fantomet", "staff", "2010-04-03");
