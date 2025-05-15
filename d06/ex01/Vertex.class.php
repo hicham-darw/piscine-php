d586b2602c2a8992375839d2b1021a0d2f946094
hicham-darw <hamoudihicham@gmail.com>
2025-05-15 18:32:28 +0100

    Vertex class completed!

--- d06/ex01/Vertex.class.php
+++ d06/ex01/Vertex.class.php
@@ -1,50 +1,53 @@
 <?php
+	require_once("../ex00/Color.class.php");
 
 class Vertex {
 
-	public static boolean $verbose = false;
+	public static bool $verbose = false;
 	private float $__x;
 	private float $__y;
 	private float $__z;
 	private float $__w = 1.0;
-	public $color = new Color([255, 255, 255]);
+	public $color ;
 
-public function __construct($arr, $colr){
-	if(isset($arr['x']) && isset($arr['y']) && isset($arr['z']) && isset($colr)){
+public function __construct($arr){
+	if(isset($arr['x']) && isset($arr['y']) && isset($arr['z'])){
 		$this->__x = $arr['x'];
 		$this->__y = $arr['y'];
 		$this->__z = $arr['z'];
-		$this->color = $colr;
+		$this->color = new Color([255, 255, 255]);
 		if(Vertex::$verbose){
-			echo "Vertex( x: $this->__x, y: $this->__y, z: $this->__z, " .
-					"w: $this->__w, $this->color ) ) constructed\n";
+			echo "Vertex( x: " . number_format($this->__x, 2) . ", y: " . 				  number_format($this->__y, 2) . ", z: " . 						    number_format($this->__z, 2) . ", w: " . 
+			number_format($this->__w, 2) . ", $this->color ) constructed\n";
 		}
 	}
 	return;
 }
 
 public function __toString(){
-	$str =  "Vertex( x: $this->__x, y: $this->__y, z: $this->__z, w: $this->__w, ";
-	if(Color::$verbose)
-		return $str . "$this->color )\n";
-	else
-		return $str . ")\n";
+	$str = "Vertex( x: " . number_format($this->__x, 2) . ", y: " . 				 number_format($this->__y,2) . ", z:" . number_format($this->__z, 2) . 			 	", w: ".number_format($this->__w, 2).", ";
+	if(Vertex::$verbose){
+		return $str . "$this->color )";
+	}else
+		return $str . ")";
 }
 
 public static function doc(){
 
-	if(file_exists("Vertex.doc.txt")){
+	if(file_exists("Vertex.doc.txt"))
 		$str = file_get_contents('Vertex.doc.txt');
-		return $str;
-	}else{
+	else
 		return false;
-	}
+	return $str;
 }
 
+public function __destruct(){
 
-
-	
-	
+	if(Vertex::$verbose){
+		echo "Vertex( x: " . number_format($this->__x, 2) . ", y: " . 				  number_format($this->__y, 2) . ", z: " . number_format($this->__z, 2) . 		    " w: " . number_format($this->__w, 2) . ", $this->color ) ) destructed\n";
+	}
+	return;
+}
 
 }
 
