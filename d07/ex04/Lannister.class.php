5a8c229d8fe93f9492b15dc4c4962066df08cc1f
hicham-darw <hamoudihicham@gmail.com>
2025-05-19 02:20:58 +0100

    create Lannister class

---
+++ d07/ex04/Lannister.class.php
@@ -0,0 +1,15 @@
+<?php
+
+class Lannister{
+
+public function my_fn(){
+	echo "With pleasure, but only ina tower in winterfall, then.\n";
+}
+
+public function my_fn2(){
+	echo "Not even if I'm drunk !\n";
+	exit();
+}
+}
+
+?>
