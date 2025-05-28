b73085bc05a7be129e86fbc5a3d1c1cb5eaf061d
hicham-darw <hamoudihicham@gmail.com>
2025-05-28 19:56:37 +0100

    fin ex05

---
+++ d07/ex05/IFighter.class.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once "test1.php";
+require_once "NightsWatch.class.php";
+
+interface IFighter {
+
+	public function fight();
+}
+
+?>
---
+++ d07/ex05/NightsWatch.class.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once "IFighter.class.php";
+
+class NightsWatch implements IFighter {
+
+	public function recruit($anstance){
+		if(method_exists($anstance, 'fight')){
+			$anstance->fight();
+		}			
+		return;
+	}
+	public function fight(){
+		return;
+	}
+
+}
+
+
+?>
