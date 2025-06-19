b628a97dfcde737a6b56688ee29cbab7c822c271
hicham-darw <hamoudihicham@gmail.com>
2025-06-19 19:12:11 +0100

    add button create new TODO

--- d09/ex03/ex02bis/todo.js
+++ d09/ex03/ex02bis/todo.js
@@ -1,1 +1,15 @@
-
+$(document).ready(function(){
+	$("#btn").click(function(){
+		let div = $('#ft_list');
+		let todo = prompt("create new TODO", "TO DO...");
+		if(todo != "" && todo != null){
+			$('#ft_list').prepend("<p>" + todo + "</p>");
+		}
+		let childs = div.children('p');
+		var i = 0;
+		while(i < childs.length){
+			childs[i].setAttribute("id", i);
+			i++;
+		}
+	});
+});
