733eb1e96f478389353c3089b964f4a5cecc0471
hicham-darw <hamoudihicham@gmail.com>
2025-06-22 03:24:52 +0100

    insert fix error list.csv

--- d09/ex04/insert.php
+++ d09/ex04/insert.php
@@ -1,8 +1,10 @@
 <?php
 	$txt = file_get_contents('list.csv');
-	$list = explode("\n", substr($txt,0, strlen($txt) - 1));
-	array_push($list, (count($list)) . "; " . $_REQUEST['q']);
+	$list = array();
+	if($txt)
+		$list = explode("\n", $txt);
+	array_push($list, (count($list) + 1) . "; " . $_REQUEST['q']);
 	$txt = implode("\n", $list);
-	file_put_contents("list.csv", $txt);
+	file_put_contents("list.csv", $txt);	
 	require('select.php');
 ?>
