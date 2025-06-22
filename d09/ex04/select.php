b0c7cbcd171b7f4576924aa7803b9ff545571537
hicham-darw <hamoudihicham@gmail.com>
2025-06-22 03:26:49 +0100

    select all todo

--- d09/ex04/select.php
+++ d09/ex04/select.php
@@ -1,8 +1,14 @@
 <?php
 
 	$txt = file_get_contents("list.csv");
-	$list = explode("\n", $txt);
-	foreach($list as $p){
-		echo '<p>' . str_replace(';', ' -', $p) . '</p>';
+	if(!$txt){
+		echo "<h1>Nothing!</h1>";
+		exit();
 	}
+	$list = explode("\n", $txt);
+	echo "<ul>";
+	foreach($list as $p){
+		echo '<li>' . str_replace("; ", " / ", $p) . '</li>';
+	}
+	echo "</ul>";
 ?>
